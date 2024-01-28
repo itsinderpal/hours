990b06b98db5fdbefda978f7a41a031683d895e9
Inderpal <inderpalsingh6763@gmail.com>
2024-01-28 17:43:36 -0500

    mongoose connection change

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,13 @@ const userRouter = require('./controllers/userRouter');
 const loginRouter = require('./controllers/loginRouter');
 
 mongoose.set('strictQuery', false)
-mongoose.connect(MONGO_URI)
-.then(() => console.log("Connected to mongodb successfully"))
-.catch((error) => console.error("error connecting to mongodb", error))
+
+const mongooseConnect =  async () => {
+   const connect = await mongoose.connect(MONGO_URI)
+   connect.then(() => console.log("Connected to mongodb successfully"))
+   connect.catch((error) => console.error("error connecting to mongodb", error))
+}
+mongooseConnect();
 
 app.use(cors())
 app.use(express.static('dist'));
